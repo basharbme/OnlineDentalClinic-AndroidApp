6301c27fd49ff26cbf13300c964b852b9394a9e1
sergioseks <u201520167@upc.edu.pe>
2019-09-27 19:41:05 -0500

    added link to patient activities

--- app/src/main/java/com/healthapps/onlinedentalclinic/controllers/activities/LoginActivity.kt
+++ app/src/main/java/com/healthapps/onlinedentalclinic/controllers/activities/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.healthapps.onlinedentalclinic.controllers.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthapps.onlinedentalclinic.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private var email: String = ""
+    private var password: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +19,19 @@ class LoginActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_login)
+
+        button_login.setOnClickListener{
+            email = editText_email.text.toString()
+            password = editText_password.text.toString()
+
+            if(email == "sergioespinal@gmail.com" && password == "sergio123"){
+                val intent = Intent(this, PatientActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        button_register.setOnClickListener {
+
+        }
     }
 }
