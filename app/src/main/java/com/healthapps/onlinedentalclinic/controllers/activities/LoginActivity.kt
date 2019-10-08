935eb5f3b005a75f89e3fe1f731debf4b65720cb
sergioseks <u201520167@upc.edu.pe>
2019-10-08 11:41:16 -0500

    added valid login

--- app/src/main/java/com/healthapps/onlinedentalclinic/controllers/activities/LoginActivity.kt
+++ app/src/main/java/com/healthapps/onlinedentalclinic/controllers/activities/LoginActivity.kt
@@ -3,12 +3,16 @@ package com.healthapps.onlinedentalclinic.controllers.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.healthapps.onlinedentalclinic.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     private var email: String = ""
     private var password: String = ""
+    private var validEmail: Boolean = false
+    private var validPassword : Boolean = false
+    private val emailPattern = Patterns.EMAIL_ADDRESS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,27 @@ class LoginActivity : AppCompatActivity() {
             email = editText_email.text.toString()
             password = editText_password.text.toString()
 
+            email.apply {
+                if(this.isBlank()){
+                    textEmailError.text = getString(R.string.textEmailError)
+                }else{
+                    textEmailError.text = ""
+                    if(validateEmail(this)){
+                        validEmail = true
+                    }else{
+                        textEmailError.text = getString(R.string.textInvalidEmail)
+                    }
+                }
+            }
+            password.apply {
+                if(this.isBlank()){
+                    textPasswordError.text = getString(R.string.textPasswordError)
+                }else{
+                    textPasswordError.text = ""
+                    validPassword = true
+                }
+            }
+
             if(email == "sergioespinal@gmail.com" && password == "sergio123"){
                 val intent = Intent(this, PatientActivity::class.java)
                 startActivity(intent)
@@ -33,5 +58,9 @@ class LoginActivity : AppCompatActivity() {
         button_register.setOnClickListener {
 
         }
+    }
+
+    private fun validateEmail(email: String): Boolean{
+        return emailPattern.matcher(email).matches()
     }
 }
